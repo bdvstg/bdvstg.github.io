75fdb5e00599e5a2a72b8313f502e6cc5bedbc03
bdvstg <bdvstg@tronc-e.com>
2020-03-31 13:35:41 +0800

    init version of build.pl
    it do 'git log' to every article (directory that contains md file)
    for retrieve all commit sha,date of article
    save it as json file (list.json)

--- build.pl
+++ build.pl
@@ -1,18 +1,45 @@
 #!/usr/bin/perl
+use JSON::PP;
 use File::Basename;
 use File::Find;
 
-sub wanted {
-    /^.*\.md/s &&
-    (($name,$path,$suffix) = fileparse($File::Find::name)) &&
-    print "$name ---- $path ---- $suffix\n";
+$json = JSON::PP->new->ascii->pretty->allow_nonref;
+
+my @files;
+find(sub {
+    /^.*\.md/s && # find all *.md in target directory (./article)
+    ($files[$#files+1] = $File::Find::name);
+}, "./article");
+
+my @list;
+for my $file (@files) {
+    ($name,$path,$suffix) = fileparse($file); # get path of directory of md file
+
+    # get commits of directory
+    my @rawdata = `git log --pretty=format:"%H,%ad" --date=format:"%Y-%m-%d %H:%M" $path`;
+
+    my @history;
+    for my $data (@rawdata) { # parse rawdata and push into history
+        chomp($data); # trim end of newline
+        my @values = split(',', $data);
+        my %cd = (
+            "sha" => $values[0], # sha(hash) code of commit
+            "date" => $values[1] # commit date
+        );
+        $history[$#history+1] = \%cd;
+    }
+    my %article = ( # combine result
+        "text" => $name,
+        "path" => "$path$name",
+        "history" => \@history
+    );
+    $list[$#list+1] = \%article;
 }
+my %output = ("list"=>\@list);
+$pretty_printed_json_text = encode_json \%output;
+#print($pretty_printed_json_text);
 
-find(\&wanted, "./");
-#for my $file ($files){
-    #print("========> $file");
-    #($name,$path,$suffix) = fileparse($file);
-    #print "name=" . $name . ",  path=" . $path . ",  suffix=" . $suffix;
-#}
-
-# command <get hash and date> 'git log --pretty=format:"%H - %ad" --date=format:"%Y-%m-%d %H:%M" article/ViewMatrix/'
+my $filename = 'list.json';
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+print $fh $pretty_printed_json_text;
+close $fh;
